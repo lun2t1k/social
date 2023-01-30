cdd1ed43e91dd42eccf1b63dda9d9170380816c6
Dmitry Krutkin <krutkin@a2design.biz>
2023-01-30 14:20:13 +0600

    Added Footer layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,34 @@
 export default function Footer(props) {
-    return;
+    return (
+        <footer className="pt-20 pb-16">
+            <div className="container-props">
+                <ul className="flex justify-center items-center gap-10">
+                    <li>
+                        <a
+                            href="/"
+                            className="px-4 py-3 text-slate-500 hover:underline hover:underline-offset-2"
+                        >
+                            About Us
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="/"
+                            className="px-4 py-3 text-slate-500 hover:underline hover:underline-offset-2"
+                        >
+                            Terms & Conditions
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="/"
+                            className="px-4 py-3 text-slate-500 hover:underline hover:underline-offset-2"
+                        >
+                            Privicy Policy
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    );
 }
