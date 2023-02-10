63f4b1307472a325d6437b9304864a4229125ba1
Dmitry Krutkin <krutkin@a2design.biz>
2023-02-10 19:47:13 +0600

    Installed axios

--- src/components/Friends/FriendsList.jsx
+++ src/components/Friends/FriendsList.jsx
@@ -1,6 +1,13 @@
+import axios from "axios";
 import FriendsItem from "./FriendsItem";
 
 export default function FriendsList(props) {
+    if (props.friends.length === 0) {
+        axios
+            .get("https://social-network.samuraijs.com/api/1.0/users")
+            .then((response) => console.log(response));
+    }
+
     let friendsList = props.friends.map((friend) => (
         <FriendsItem
             key={friend.id}
