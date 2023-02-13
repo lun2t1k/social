61d1e51e394d9fc340d8e96a576b9a46fc906e0e
Dmitry Krutkin <krutkin@a2design.biz>
2023-02-13 14:18:47 +0600

    Added FriendsList Class Component

--- src/components/Friends/FriendsList.jsx
+++ src/components/Friends/FriendsList.jsx
@@ -1,22 +1,50 @@
+import React from "react";
 import axios from "axios";
 import FriendsItem from "./FriendsItem";
 
-export default function FriendsList(props) {
-    if (props.friends.length === 0) {
+class FriendsList extends React.Component {
+    componentDidMount() {
         axios
             .get("https://social-network.samuraijs.com/api/1.0/users")
             .then((response) => console.log(response));
     }
-
-    let friendsList = props.friends.map((friend) => (
-        <FriendsItem
-            key={friend.id}
-            id={friend.id}
-            name={friend.name}
-            avatar={friend.avatar}
-            status={friend.status}
-        />
-    ));
-
-    return <ul>{friendsList}</ul>;
+    render() {
+        return (
+            <>
+                {this.props.friends.map((friend) => {
+                    return (
+                        <FriendsItem
+                            key={friend.id}
+                            id={friend.id}
+                            name={friend.name}
+                            avatar={friend.avatar}
+                            status={friend.status}
+                        />
+                    );
+                })}
+            </>
+        );
+    }
 }
+
+export default FriendsList;
+
+// export default function FriendsList(props) {
+//     if (props.friends.length === 0) {
+//         axios
+//             .get("https://social-network.samuraijs.com/api/1.0/users")
+//             .then((response) => console.log(response));
+//     }
+
+//     let friendsList = props.friends.map((friend) => (
+//         <FriendsItem
+//             key={friend.id}
+//             id={friend.id}
+//             name={friend.name}
+//             avatar={friend.avatar}
+//             status={friend.status}
+//         />
+//     ));
+
+//     return <ul>{friendsList}</ul>;
+// }
